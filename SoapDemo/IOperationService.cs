a4137ad727dc995a36157198cf27da51449e6927
Heckthor Marin <heckthor.marin.71@gmail.com>
2021-06-02 23:27:22 -0500

    Agregar archivos de proyecto.

---
+++ SoapDemo/IOperationService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SoapDemo
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IOperationService
+    {
+
+        [OperationContract]
+        string OperateValues(int operator1, int operator2);
+
+        [OperationContract]
+        string OperateObject(OperationElements op);
+
+        // TODO: agregue aquí sus operaciones de servicio
+    }
+
+
+    // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+    [DataContract]
+    public class OperationElements
+    {
+
+        [DataMember]
+        public int OpA { get; set; }
+
+        [DataMember]
+        public int OpB { get; set; }
+
+    }
+}
---
+++ SoapDemo/OperationService.svc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SoapDemo
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class OperationService : IOperationService
+    {
+        public string OperateValues(int operator1, int operator2)
+        {
+            var result = operator1 + operator2;
+            return string.Format("The Result Is: {0}", result);
+        }
+
+        public string OperateObject(OperationElements elements)
+        {
+            var result = elements.OpA + elements.OpB;
+            return string.Format("The Result Is: {0}", result);
+        }
+    }
+}
